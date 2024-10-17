71288fc3d883d721139de09989404149ea1f70f9
estebanDC <esteban.da_costa@outlook.fr>
2024-10-17 13:49:15 +0200

    CRA-1: Add Task entity, table, and DTO
    Introduced a new `TaskEntity` class with corresponding `Tasks` table and `TaskDTO` data structure. This addition includes fields for task name, description, completion status, and an optional associated team.

---
+++ src/main/kotlin/com/crackling/databases/entities/TaskEntity.kt
@@ -0,0 +1,18 @@
+package com.crackling.databases.entities
+
+import com.crackling.databases.dtos.TaskDTO
+import com.crackling.databases.tables.Tasks
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class TaskEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<TaskEntity>(Tasks)
+
+    var name by Tasks.name
+    var description by Tasks.description
+    var completed by Tasks.completed
+    var team by TeamEntity referencedOn Tasks.teamId
+
+    fun toDTO(addTeam: Boolean = false) = TaskDTO(name, description, completed, if (addTeam) team.toDTO() else null)
+}
